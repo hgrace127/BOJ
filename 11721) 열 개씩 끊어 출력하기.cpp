468ae0fcbf1d9b7ecc76d0a85b4415f4a64b1893
Seungju Kim <hgrace127@naver.com>
2017-02-16 23:34:48 +0900

    BOJ: Q. 소문자와 대문자로만 이루어진 길이 N인 단어가 주어진다. 한줄에 10글자씩 끊어서 출력하는 프로그램을 작성하시오. 길이 100을 넘지 않는다. 길이가 0ㅇ인 단어는 주어지지 않는다.
    문자열 입력받고 문자를 출력한다. 10의 배수가 되면 다음 줄로 넘어간다.
    ---------------------------------------------------------------------------------------------------------------------------------------

---
+++ 11721) 열 개씩 끊어 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+	char str[101];
+	scanf("%s", str);
+	for (int i = 1; i <= strlen(str); i++) {
+		printf("%c", str[i-1]);
+		if (i % 10 == 0)
+			printf("\n");
+	}
+	return 0;
+}
