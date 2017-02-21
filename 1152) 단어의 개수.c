b341e623fb212debc479363fdb20e76edcbb9557
Seungju Kim <hgrace127@naver.com>
2017-02-21 16:40:03 +0900

    BOJ: (틀렸습니다.) 문자열 처리
    Q. 영어 대소문자와 띄어쓰기만으로 이루어진 문장이 주어진다. 이 문장에는 몇 개의 단어가 있을까?
    이를 구하는 프로그램을 작성하시오. 단, 단어는 띄어쓰기 하나로 구분된다고 생각한다.
    
    첫 줄에 영어 대소문자와 띄어쓰기로 이루어진 문장이 주어진다. 이 문장의 길이는 1,000,000을 넘지 않는다.
    ex) The Curious Case of Benjamin Button | 6
    
    공백
    공백 단어
    공백 단어 공백
    단어
    단어 공백
    단어 공백 단어
    VS로 출력했을 때는 정답인데 BOJ에서 정답을 왜 틀렸다고 나올까?
    
    ---------------------------------------------------------------------------------------------------------------------------------------
    앞선 커밋에서 동적할당을 한 후 배열을 선언하는 아이디어는 에러가 발생한다.
    배열을 선언할 때 배열의 크기에 변수를 넣을 수 없다. 
    상수만 넣을 수 있다.

--- 1152) 단어의 개수.c
+++ 1152) 단어의 개수.c
@@ -2,27 +2,27 @@
 
 int main() {
 
-	int str[1000000];
+	char str[1000001];
 
-	int count = 1;
+	int count = 0;
 	int len, i;
 
 	fgets(str, sizeof(str), stdin);
-	for (i = 0; i < sizeof(str); i++) {
-		if (str[i] == '\0') {
-			len = i - 1;	// 길이에서 \0 제거
-			i++;
-		}
+
+	if (str[0] == '\n') {
+		count = 0;
+		return 0;
+	}
+	else if (str[0] != ' ' && str[0] != '\n') {
+		count = 1;
 	}
 
-	for (i = 0; i < len; i++) {
-		if (str[i] == ' ') {
+	for (i = 1; i <= sizeof(str) - 1; i++) {
+		
+		if (str[i - 1] == ' ' && str[i] != '\n' && str[i] != ' ') {
 			count++;
 		}
 	}
-
-	if (str[0] == '\n')
-		count = 0;
 
 	printf("%d", count);
 	return 0;
