115d7913951928cadeac507bc4b95abeaef4812d
Seungju Kim <hgrace127@naver.com>
2017-02-28 21:10:01 +0900

    BOJ: 1152 문자열 처리
    Q. 영어 대소문자와 띄어쓰기만으로 이루어진 문장이 주어진다.
    이 문장에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오. 단, 단어는 띄어쓰기 하나로 구분된다고 생각한다.
    첫 줄에 영어 대소문자와 띄어쓰기로 이루어진 문장이 주어진다. 이 문장의 길이는 1,000,000을 넘지 않는다.
    
    이 문제를 풀면서 문자열을 입력 받는 함수를 다시 알아봤다.
    fgets 함수는 '\n' 개행문자까지 입력을 받는다. 주로 파일에서 입력받을 때 사용한다.
    gets 함수는 개행문자는 입력 받지 않지만 버퍼 오버플로우가 될 가능성이 있기 때문에 사용하지 않는 것이 좋다.
    gets_s 함수는 컴파일러에 따라 사용 유무가 갈린다.
    
     좋은 아이디어는 fgets를 입력 받고 '\n'자리를 '\0'으로 바꾸는 것이다. (12 ~ 13 줄)
    strchr는 문자열에 특정 문자가 포함 돼 있으면 그 자리

--- 1152) 단어의 개수.c
+++ 1152) 단어의 개수.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
 
 	char str[1000001];
-
+	char* p;
 	int count = 0;
-	int len, i;
+	int i;
 
-	fgets(str, sizeof(str), stdin);
-
-	if (str[0] == '\n') {
-		count = 0;
-		return 0;
-	}
-	else if (str[0] != ' ' && str[0] != '\n') {
+	// fgets(str, sizeof(str), stdin); -> \n까지 입력 됨
+	// gets_s(str, sizeof(str)); -> 컴파일러가 인식 못할 수 있음
+	fgets(str, sizeof(str) - 1, stdin);
+	if ((p = strchr(str, '\n')) != NULL) *p = '\0';
+	
+	// 공백 
+	// 공백 단어 
+	// 공백 단어 공백
+	// 단어 
+	// 단어 공백 
+	// 단어 공백 단어
+	if (str[0] != ' ')
 		count = 1;
-	}
 
-	for (i = 1; i <= sizeof(str) - 1; i++) {
-		
-		if (str[i - 1] == ' ' && str[i] != '\n' && str[i] != ' ') {
+	for (i = 1; i < strlen(str); i++) {		
+		if (str[i - 1] == ' ' && str[i] != ' ') {
 			count++;
 		}
 	}
-
 	printf("%d", count);
 	return 0;
 }
