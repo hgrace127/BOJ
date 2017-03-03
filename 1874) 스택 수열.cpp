d50310a7086865595b8516cbbdaad08261d5379e
Seungju Kim <hgrace127@naver.com>
2017-03-03 23:43:59 +0900

    BOJ: 1874 스택 사용하기
    이번에는 함수를 사용하지 않고 main 함수 안에 기능을 모두 넣어 코드를 만들었다.

--- 1874) 스택 수열.cpp
+++ 1874) 스택 수열.cpp
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include <cstring>
-typedef struct _STACK {
-	int data;
-}STACK;
-STACK s[100000];
+int s[100000];
 int userArr[100000] = { 0, };
 char ans[200001];
 int idx = -1, ansIdx = 0, aIdx = 0;
-void push(int _data);
-int pop();
-int top();
-int empty();
 int main() {
 	unsigned int N, i = 0, len;
 	scanf("%d", &N);
@@ -19,33 +12,24 @@ int main() {
 		scanf("%d", &userArr[i]);
 	}
 	for (i = 1; i <= N; i++) {
-		push(i);
-		while (idx >= 0 && top() == userArr[aIdx]) {
-			pop();
+		// push
+		ans[ansIdx++] = '+';
+		s[++idx] = i;
+		while (idx >= 0 && (s[idx] == userArr[aIdx])) {
+			// pop
+			ans[ansIdx++] = '-';
+			s[idx--] = -1;
 			aIdx++;
 		}
 	}
 	len = strlen(ans);
-	if (empty()) {
+	//IsEmpty
+	if (idx == -1) {
 		for (i = 0; i < len; i++)
 			printf("%c\n", ans[i]);
 	}
-	else	printf("NO");
-
+	else {
+		printf("NO");
+	}
 	return 0;
-}
-int empty() {
-	return (idx == -1);
-}
-void push(int _data) {
-	ans[ansIdx++] = '+';
-	s[++idx].data = _data;
-}
-int pop() {
-	ans[ansIdx++] = '-';
-	s[idx].data = -1;
-	return s[idx--].data;
-}
-int top() {
-	return s[idx].data;
 }
